49791da386fe0c3d420356609b2a181ac234bb15
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-11 17:23:17 +0530

    Take mobile-first approach and add style guide colours
    Desktop first approach led to layout issues on
    both mobile and desktop. Colours were incorrect.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       fontFamily: {
         frontend: ['"DM Sans", sans-serif'],
       },
+      colors: {
+        frontend: {
+          purple: {
+            100: "hsl(254, 88%, 90%)",
+            500: "hsl(256, 67%, 59%)",
+          },
+          yellow: {
+            100: "hsl(31, 66%, 93%)",
+            500: "hsl(39, 100%, 71%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
